c4d0b8f1d0df7635d2e69610a911763373109e44
faridmehdiyev3000 <faridm3001@gmail.com>
2022-10-06 08:36:05 -0700

    exercise1

---
+++ src/az/coders/exercise/PrimeNumber.java
@@ -0,0 +1,25 @@
+package az.coders.exercise;
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+
+
+        int n = 5, c = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                c++;
+
+            }
+
+        }
+
+        if (c == 2) {
+            System.out.println(n + "is a prime number");
+        } else {
+            System.out.println(n + "is not a prime number");
+        }
+
+
+    }
+}
+
