d67af5923ba107efabbb8b4ddc4135fd5eb28d0c
faridmehdiyev3000 <faridm3001@gmail.com>
2022-10-06 08:38:34 -0700

    exercise1

--- src/az/coders/exercise/PrimeNumber.java
+++ src/az/coders/exercise/PrimeNumber.java
@@ -4,7 +4,7 @@ public class PrimeNumber {
     public static void main(String[] args) {
 
 
-        int n = 5, c = 0;
+        int n = 100, c = 0;
         for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
                 c++;
