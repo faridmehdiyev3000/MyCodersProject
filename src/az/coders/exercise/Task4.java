043eb180864b079d0c069403ca5de60c1c7414cb
faridmehdiyev3000 <faridm3001@gmail.com>
2022-10-13 08:45:29 -0700

    exercise5

--- src/az/coders/exercise/Task4.java
+++ src/az/coders/exercise/Task4.java
@@ -6,7 +6,7 @@ public class Task4 {
         for (int i = 0; i < a.length;i++){
             for (int j = i+1; j < a.length;j++){
                 int temp=0;
-                if (a[i] < a[j]) {
+                if (a[i] > a[j]) {
                     temp = a[i];
                     a[i] = a[j];
                     a[j] = temp;
