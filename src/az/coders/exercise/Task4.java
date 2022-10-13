ff69dcdd0d35144b5f368da81994045e36179a58
faridmehdiyev3000 <faridm3001@gmail.com>
2022-10-13 08:24:51 -0700

    exercise5

--- src/az/coders/exercise/Task4.java
+++ src/az/coders/exercise/Task4.java
@@ -7,7 +7,6 @@ public class Task4 {
             for (int j = i+1; j < a.length;j++){
                 int temp=0;
                 if (a[i] < a[j]) {
-                    // Swapping
                     temp = a[i];
                     a[i] = a[j];
                     a[j] = temp;
