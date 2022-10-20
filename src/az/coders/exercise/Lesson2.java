ab764f0082d5f9ee50fea613df23e1f3a5e61add
faridmehdiyev3000 <faridm3001@gmail.com>
2022-10-20 09:20:29 -0700

    exercise6

--- src/az/coders/exercise/Lesson2.java
+++ src/az/coders/exercise/Lesson2.java
@@ -7,14 +7,36 @@ public class Lesson2 {
         int b = new Scanner(System.in).nextInt();
         int c = new Scanner(System.in).nextInt();
 
-for(int i=0,j=0;i<=b;i+=30){
-    if(b==i){
-        System.out.println(i);
-    }
-}
-        }
-        }
+        if (b - c == 1 || c - b == 1) {
+            System.out.println("3o derece");
 
+        } else if (b - c == 2 || c - b == 2) {
+            System.out.println("60 derece");
+
+        } else if (b - c == 3 || c - b == 3) {
+            System.out.println("90 derece");
+
+        } else if (b - c == 4 || c - b == 4) {
+            System.out.println("120 derece");
+        } else if (b - c == 5 || c - b == 5) {
+            System.out.println("150 derece");
+        } else if (b - c == 6 || c - b == 6) {
+            System.out.println("180 derece");
+        } else if (b - c == 7 || c - b == 7) {
+            System.out.println("210 derece");
+        } else if (b - c == 8 || c - b == 8) {
+            System.out.println("240 derece");
+        } else if (b - c == 9 || c - b == 9) {
+            System.out.println("270 derece");
+        } else if (b - c == 10 || c - b == 10) {
+            System.out.println("300 derece");
+        } else if (b - c == 11 || c - b == 11) {
+            System.out.println("330 derece");
+        } else if (b - c == 0 || c - b == 0) {
+            System.out.println("0 derece");
+        }
+        }
+    }
 
 
 
