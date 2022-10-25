a3ac9cd661eba8ec0e7be57573bdebbb42cfb6ee
faridmehdiyev3000 <faridm3001@gmail.com>
2022-10-25 08:53:33 -0700

    exercise6

--- src/az/coders/exercise/Farid2.java
+++ src/az/coders/exercise/Farid2.java
@@ -3,12 +3,14 @@ package az.coders.exercise;
 public class Farid2 {
     public static void main(String[] args) {
         int cem = 1;
-        StringBuilder sb2 = new StringBuilder("animalll");
+        StringBuilder sb2 = new StringBuilder("animaalll");
         for (int i = 0; i < sb2.length(); i++) {
             for (int j = 1; i < sb2.length(); j++) {
                 if (sb2.charAt(i) == sb2.charAt(j)) {
                     cem += 1;
                     System.out.println(cem);
+                    System.out.println(sb2.charAt(i));
+
 
 
 
