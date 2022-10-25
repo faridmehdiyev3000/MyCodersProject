18bf4093f1c6c7355dc1e648013cac176aeb1e64
faridmehdiyev3000 <faridm3001@gmail.com>
2022-10-25 08:44:46 -0700

    exercise6

--- src/az/coders/exercise/Farid1.java
+++ src/az/coders/exercise/Farid1.java
@@ -6,16 +6,17 @@ public class Farid1 {
     public static void main(String[] args) {
         int cem = 0;
         Random a = new Random();
-        int i = a.nextInt(1, 9);
-        int f = a.nextInt(1, 9);
-        int y = a.nextInt(1, 9);
+        int i = a.nextInt(1, 10);
+        int f = a.nextInt(1, 10);
+        int y = a.nextInt(1, 10);
         int[] t = {i, f, y};
         System.out.print(i);
         System.out.print(f);
         System.out.print(y);
         System.out.print(" ");
-        cem += 100 * i + 10 * f + y + 1;
-        System.out.println(cem);
+        cem += 100 * i + 10 * f + y ;
+        int cem2=100 * i + 10 * f + y +1;
+        System.out.println(cem2);
         int[] l = {i, f, y + 1};
         int p;
         for (p = 0; p < l.length; p++) {
--- src/az/coders/exercise/Farid2.java
+++ src/az/coders/exercise/Farid2.java
@@ -3,12 +3,13 @@ package az.coders.exercise;
 public class Farid2 {
     public static void main(String[] args) {
         int cem = 1;
-        StringBuilder sb2 = new StringBuilder("animal");
+        StringBuilder sb2 = new StringBuilder("animalll");
         for (int i = 0; i < sb2.length(); i++) {
             for (int j = 1; i < sb2.length(); j++) {
                 if (sb2.charAt(i) == sb2.charAt(j)) {
                     cem += 1;
                     System.out.println(cem);
+
 
 
                 }
