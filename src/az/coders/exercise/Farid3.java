23b8283de1caca65724beceaa49d1b55d08dc25d
faridmehdiyev3000 <faridm3001@gmail.com>
2022-11-01 09:00:36 -0700

    exercise6

--- src/az/coders/exercise/Farid3.java
+++ src/az/coders/exercise/Farid3.java
@@ -2,7 +2,8 @@ package az.coders.exercise;
 
 public class Farid3 {
     public static void main(String[] args) {
-        suffling("codeleet",new int[]{4,5,6,7,0,1,2,3});
+        String res=suffling("codeleet",new int[]{4,5,6,7,0,1,2,3});
+        System.out.println(res);
     }
     public static String suffling(String s, int[] indices) {
         String res = " ";
