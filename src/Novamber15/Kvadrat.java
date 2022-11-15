acb388422ded26c130de70e7535ea44a3281f193
faridmehdiyev3000 <faridm3001@gmail.com>
2022-11-15 07:21:01 -0800

    exercise6

--- src/Novamber15/Kvadrat.java
+++ src/Novamber15/Kvadrat.java
@@ -5,7 +5,7 @@ public class Kvadrat implements HendesiFigur{
 
     @Override
     public double doubleSaheninTapilmasi(int a, int h) {
-        double f=a*a;
+        double f=Math.pow(a,a);
         return f;
     }
 }
