c9b32d09c6e9f24ffdc3bfec1b6c08e102f37886
faridmehdiyev3000 <faridm3001@gmail.com>
2022-11-15 07:15:52 -0800

    exercise6

--- src/Novamber15/ArrayForm.java
+++ src/Novamber15/ArrayForm.java
@@ -3,13 +3,13 @@ package Novamber15;
 public class ArrayForm {
     public static void main(String[] args) {
         Daire daire = new Daire();
-        double s = daire.doubleSaheninTapilmasi(2, 4);
+
         Kvadrat kvadrat = new Kvadrat();
-        double r = kvadrat.doubleSaheninTapilmasi(2, 4);
+
         Ucbucaq ucbucaq = new Ucbucaq();
-        double g = ucbucaq.doubleSaheninTapilmasi(2, 4);
+
         Dordbucaq dordbucaq = new Dordbucaq();
-        double l = dordbucaq.doubleSaheninTapilmasi(2, 4);
+
         double[] arr = {daire.doubleSaheninTapilmasi(2, 4), kvadrat.doubleSaheninTapilmasi(2, 4), ucbucaq.doubleSaheninTapilmasi(2, 4),
                 dordbucaq.doubleSaheninTapilmasi(2, 4)};
         for (int i = 0; i < arr.length; i++) {
